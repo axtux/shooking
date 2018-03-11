d58e3fb66e7441e2046bbc2d5b9e72e96a42b8ec
benjamin <benjamin.nicodeme@icloud.com>
2018-03-11 12:36:17 +0100

    First Research functionnality, has to be adapted for DB interface

---
+++ src/be/ac/ulb/infof307/g10/ResearchTest.java
@@ -0,0 +1,35 @@
+package be.ac.ulb.infof307.g10;
+
+import junit.framework.TestCase;
+
+import java.util.*;
+
+/**
+ * Research Tests
+ * @author Benjamin Nicodeme
+ *
+ */
+
+public class ResearchTest extends TestCase {
+
+    private ArrayList <Object> l;
+    private Object p;
+    public void setUp() throws Exception {
+        super.setUp();
+        l = new ArrayList<>();
+        p = new Object();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testGetStoreWithProducts() {
+        Research r = new Research();
+        r.getStoreWithProducts(l);
+    }
+
+    public void testGetStoreWithProduct() {
+        Research r = new Research();
+        r.getStoreWithProduct(p);
+    }
+}
