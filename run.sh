b23a44e05761a17228cc8f080404ba5415f7b819
benjamin <benjamin.nicodeme@icloud.com>
2018-04-09 12:52:06 +0200

    pom.xml updated to be able to produce a jar with dependencies + run.sh edited for jar commands

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-mvn exec:java -Dexec.mainClass="be.ac.ulb.infof307.g10.Main"
+#mvn exec:java -Dexec.mainClass="be.ac.ulb.infof307.g10.Main"
+
+mvn clean compile assembly:single
+java -jar target/groupe10-1.0-SNAPSHOT-jar-with-dependencies.jar
