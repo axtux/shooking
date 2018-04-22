49f2e0c993aab3b90e545d4df6424eac54514534
Marco Salemi <Vondutchstyleas@hotmail.com>
2018-04-22 18:08:30 +0200

    Menu cleaning

--- src/be/ac/ulb/infof307/g10/controllers/MenuController.java
+++ src/be/ac/ulb/infof307/g10/controllers/MenuController.java
@@ -6,6 +6,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import be.ac.ulb.infof307.g10.Main;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
@@ -19,22 +20,22 @@ public class MenuController implements Initializable{
     }
 
     @FXML
-    public void goToMap(){
+    public void goToMap(ActionEvent event){
         Main.getInstance().goToMap();
     }
 
     @FXML
-    public void goToShoppingList(){
+    public void goToShoppingList(ActionEvent event){
         Main.getInstance().goToShoppingList();
     }
 
     @FXML
-    public void goToLogin(){
+    public void goToLogin(ActionEvent event){
         Main.getInstance().goToLogin();
     }
 
     @FXML
-    public void exit(){
+    public void exit(ActionEvent event){
         Main.getInstance().exit();
     }
 
