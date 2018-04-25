2caf5634b4c1f88041d2ac4665343c4e5a68549b
Axtux <axtux@hotmail.com>
2018-04-26 00:18:52 +0200

    exception message and cause

--- src/be/ac/ulb/infof307/g10/models/exceptions/UserAlreadyExistException.java
+++ src/be/ac/ulb/infof307/g10/models/exceptions/UserAlreadyExistException.java
@@ -1,5 +1,22 @@
 package be.ac.ulb.infof307.g10.models.exceptions;
 
 public class UserAlreadyExistException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public UserAlreadyExistException() {
+		super();
+	}
+
+	public UserAlreadyExistException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public UserAlreadyExistException(String message) {
+		super(message);
+	}
+
+	public UserAlreadyExistException(Throwable cause) {
+		super(cause);
+	}
 
 }
