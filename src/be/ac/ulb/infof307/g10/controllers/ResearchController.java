f44d71aa1225d9c96da9184580705d230ae459f2
DESKTOP-9CC45F6\simon <simon.goisse@gmail.com>
2018-05-05 23:13:00 +0200

    ca fonctionne faut rendre plus beau et refactorer

--- src/be/ac/ulb/infof307/g10/controllers/ResearchController.java
+++ src/be/ac/ulb/infof307/g10/controllers/ResearchController.java
@@ -24,23 +24,42 @@ public class ResearchController  {
 	
 	@FXML
 	private TextField productTF;
+	
+	private double priceForShoppingList;
 
 	@FXML
 	private TextArea shopTA;
 
 	public void research(ActionEvent actionEvent) {
+		double total=0.0;
 		List<Shop> shopWithAllArticles = new ArrayList<Shop>();
+		ArrayList priceForShoppingListAllShops = new ArrayList();
 		Set<Product> products = ShoppingListController.getProducts().keySet();
 		List<Shop> shopList = Database.getAllShops();
 		for (Shop s : shopList) {
+			total =0.0;
 			Set<Product> productInAShop = s.getStock().getProducts();
 			if(!Collections.disjoint(productInAShop, products)) {
 				shopWithAllArticles.add(s);
+				 for (Map.Entry<Product, Integer> entry : ShoppingListController.getProducts().entrySet()) {
+					 
+					 System.out.print(s.getStock().getPrice(entry.getKey())+ "\n");
+					 
+						total += (((s.getStock().getPrice(entry.getKey()))* entry.getValue()));//prix product * quantity
+					 //action.accept(entry.getKey(), entry.getValue());
+							
+				}
+				
+				 priceForShoppingListAllShops.add(total);
 			}
-		}
+			
+					
+			     }
+			 
+		
 		String shopNames = "";
 		for (int i =0 ; i< shopWithAllArticles.size(); ++i) {
-			shopNames += shopWithAllArticles.get(i).getName() + "\n";
+			shopNames += shopWithAllArticles.get(i).getName() + String.valueOf(priceForShoppingListAllShops.get(i))+"\n";
 		}
 		shopTA.setText(shopNames);
 	}
