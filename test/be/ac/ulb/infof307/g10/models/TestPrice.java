877e5451ce718bb0c63dc2ee69a185d9ca53dc10
Axtux <axtux@hotmail.com>
2018-05-09 20:29:02 +0200

    one assert per test

--- test/be/ac/ulb/infof307/g10/models/TestPrice.java
+++ test/be/ac/ulb/infof307/g10/models/TestPrice.java
@@ -1,19 +1,23 @@
 package be.ac.ulb.infof307.g10.models;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestPrice {
 
 	@Test
-	public void test_001_toString() {
-		String price1 = Price.toString(199);
-		String price2 = Price.toString(1);
-		String price3 = Price.toString(10);
-		assertEquals(price1, "1,99€");
-		assertEquals(price2, "0,01€");
-		assertEquals(price3, "0,10€");
+	public void test_001_1toString() {
+		Assert.assertEquals("0,01€", Price.toString(1));
+	}
+
+	@Test
+	public void test_001_10toString() {
+		Assert.assertEquals("0,10€", Price.toString(10));
+	}
+
+	@Test
+	public void test_001_199toString() {
+		Assert.assertEquals("1,99€", Price.toString(199));
 	}
 
 }
