9495be6d82e11f2d63cf01a0dd8394e1b7ca1d2b
Axtux <axtux@hotmail.com>
2018-05-18 22:01:59 +0200

    add TestProduct, do not throw NullPointerException but IllegalArgumentException

---
+++ test/be/ac/ulb/infof307/g10/models/TestProduct.java
@@ -0,0 +1,81 @@
+package be.ac.ulb.infof307.g10.models;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestProduct {
+
+	@Test(expected = IllegalArgumentException.class)
+	public void nullNameException() {
+		new Product(null, 1, "unit");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void emptyNameException() {
+		new Product("", 1, "unit");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void spaceNameException() {
+		new Product(" ", 1, "unit");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void spacesNameException() {
+		new Product("   ", 1, "unit");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void zeroSizeException() {
+		new Product("name", 0, "unit");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void negativeSizeException() {
+		new Product("name", -1, "unit");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void nullSizeUnitException() {
+		new Product("name", 1, null);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void emptySizeUnitException() {
+		new Product("name", 1, "");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void spaceSizeUnitException() {
+		new Product("name", 1, " ");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void spacesSizeUnitException() {
+		new Product("name", 1, "   ");
+	}
+
+	@Test
+	public void name() {
+		Product p = new Product("name", 1, "unit");
+		Assert.assertEquals("name", p.getName());
+	}
+
+	@Test
+	public void size() {
+		Product p = new Product("name", 1, "unit");
+		Assert.assertEquals(1, p.getSize());
+	}
+
+	@Test
+	public void unit() {
+		Product p = new Product("name", 1, "unit");
+		Assert.assertEquals("unit", p.getSizeUnit());
+	}
+
+	@Test
+	public void fullName() {
+		Product p = new Product("name", 1, "unit");
+		Assert.assertEquals("name (1unit)", p.getFullName());
+	}
+}
