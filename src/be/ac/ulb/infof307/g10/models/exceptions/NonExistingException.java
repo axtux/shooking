c0eef903d2de51b209e1cf7a662780bfd823c52a
pierre <pdanhier@ulb.ac.be>
2018-05-22 19:05:30 +0200

    Doc on the exceptions

--- src/be/ac/ulb/infof307/g10/models/exceptions/NonExistingException.java
+++ src/be/ac/ulb/infof307/g10/models/exceptions/NonExistingException.java
@@ -1,5 +1,8 @@
 package be.ac.ulb.infof307.g10.models.exceptions;
 
+/**
+ * Exception raised when an object does not exist
+ */
 public class NonExistingException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
