e96210b418e7dcb30083fa9ef91791cc46fa45cc
benjamin <benjamin.nicodeme@icloud.com>
2018-05-22 23:13:01 +0200

    ShoppingList doc updated

--- src/be/ac/ulb/infof307/g10/models/ShoppingList.java
+++ src/be/ac/ulb/infof307/g10/models/ShoppingList.java
@@ -3,7 +3,7 @@ package be.ac.ulb.infof307.g10.models;
 import javax.persistence.Entity;
 
 /**
- * Shopping list object, used to remove abstraction of ProductsQuantity
+ * Shopping list object, is actually a ProductsQuantity but with a better name to ease developers comprehension
  */
 @Entity
 public class ShoppingList extends ProductsQuantity {
